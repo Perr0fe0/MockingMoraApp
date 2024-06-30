b98f70908b8d5f309010e9f1ba6188aed5e87cb1
Perr0fe0 <83041787+Perr0fe0@users.noreply.github.com>
2024-06-30 14:14:47 -0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
         <div className="bg-white p-8 m-8 rounded-md">
           <h1 className="text-4xl text-center">Molestador de Mora</h1>
           <div id="imgShow" className="mt-4 hidden">
-            <img className="rounded-md" src="src\assets\laugh-point.gif" />
+            <img className="rounded-md" src="src\assets\images\laugh-point.gif" />
           </div>
           <h2 id="congrat" className="text-2xl text-center my-10">
             Pulse el botón para molestar a Mora
