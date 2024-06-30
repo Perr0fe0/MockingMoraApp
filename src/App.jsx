ad34eced359d0d9a5cc3fab8b9e1d37ffe788e59
Perr0fe0 <83041787+Perr0fe0@users.noreply.github.com>
2024-06-30 17:05:43 -0400

    desktop completado

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ const App = () => {
   const mocking = new Audio(song)
   const songButton = () =>{
     document.getElementById("congrat").innerHTML = "Te has burlado de Mora exitosamente!"
-    document.getElementById("imgShow").className = " mt-4 "
+    document.getElementById("boton").innerHTML = "Seguir burlandote"
+    document.getElementById("imgShow").className = " rounded-md "
     mocking.play()
 
   }
@@ -17,16 +18,16 @@ const App = () => {
 
   return (
     <>
-      <main>
-        <div className="bg-white p-8 m-8 rounded-md">
+      <main className='md:flex md:flex-col md:items-center'>
+        <div className="bg-white md:w-3/6 p-8 m-8 rounded-md">
           <h1 className="text-4xl text-center">Molestador de Mora</h1>
-          <div id="imgShow" className="mt-4 hidden">
-            <img className="rounded-md" src="https://th.bing.com/th/id/OIP.EiQDDaQrsKyY6s7EWewGpwAAAA?rs=1&pid=ImgDetMain" alt="ola" />
+          <div  className="mt-4 flex justify-center ">
+            <img id="imgShow" className="rounded-md hidden " src="https://th.bing.com/th/id/OIP.EiQDDaQrsKyY6s7EWewGpwAAAA?rs=1&pid=ImgDetMain" alt="ola" />
           </div>
           <h2 id="congrat" className="text-2xl text-center my-10">
-            Pulse el botón para molestar a Mora
+            Pulse el botón para <strong>molestar</strong> a Mora
           </h2>
-          <button onClick={songButton} className="border-2 w-[100%] border-black rounded-lg p-4">Molestar</button>
+          <button id='boton' onClick={songButton} className="border-2 w-[100%] border-black rounded-lg p-4 hover:bg-sky-500 hover:text-white font-black text-2xl transition-all">Molestar</button>
         </div>
       </main>
     </>
